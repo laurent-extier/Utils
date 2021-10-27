302f677ee949036c9adb0e462254e37201cac05e
Laurent Extier <laurent.extier@fr.ibm.com>
2021-10-27 20:06:14 +0200

    Add new alias.

--- scripts/kubernetes/kube-alias.sh
+++ scripts/kubernetes/kube-alias.sh
@@ -10,3 +10,13 @@ echo 'alias ka="k apply -f "' >>~/.bashrc
 echo 'alias ke="k edit "' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 echo 'source <(kubectl completion bash)'
+
+# setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+source <(kubectl completion bash)
+# add autocomplete permanently to your bash shell.
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+# enable completion for k aliases
+alias watch='watch ' >> ~/.bashrc
+
+
