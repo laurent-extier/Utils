f4f242c5573b4f97d1a658831f7425f1e4776391
Laurent Extier <laurent.extier@fr.ibm.com>
2021-11-26 14:47:22 +0100

    Add alias for Kubernetes.

--- scripts/kubernetes/kube-alias.sh
+++ scripts/kubernetes/kube-alias.sh
@@ -2,21 +2,16 @@
 
 echo 'alias k=kubectl.exe' >>~/.bashrc
 echo 'alias kg="k get "' >>~/.bashrc
-echo 'alias kgp=kg pods' >>~/.bashrc
-echo 'alias kgd=kg deployment' >>~/.bashrc
+echo 'alias kgp="kg pod "' >>~/.bashrc
+echo 'alias kgd="kg deployment "' >>~/.bashrc
 echo 'alias krm="k delete "' >>~/.bashrc
+echo 'alias krmp="krm pod "' >>~/.bashrc
+echo 'alias krmd="krm deployment "' >>~/.bashrc
 echo 'alias kd="k describe "' >>~/.bashrc
+echo 'alias kdp="kd pod "' >>~/.bashrc
+echo 'alias kdd="kd deployment "' >>~/.bashrc
 echo 'alias ka="k apply -f "' >>~/.bashrc
 echo 'alias ke="k edit "' >>~/.bashrc
-echo 'complete -F __start_kubectl k' >>~/.bashrc
-echo 'source <(kubectl completion bash)'
 
-# setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+complete -F __start_kubectl k
 source <(kubectl completion bash)
-# add autocomplete permanently to your bash shell.
-echo "source <(kubectl completion bash)" >> ~/.bashrc
-
-# enable completion for k aliases
-alias watch='watch ' >> ~/.bashrc
-
-
